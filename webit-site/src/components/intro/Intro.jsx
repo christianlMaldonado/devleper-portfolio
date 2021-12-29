bdeaeae329c82ea067f61ed4aa132447c399b0ca
Christian Maldonado <41269975+christianlMaldonado@users.noreply.github.com>
2021-12-29 11:29:08 -0800

    Changes to NPX

---
+++ webit-site/src/components/intro/Intro.jsx
@@ -0,0 +1,24 @@
+import "./intro.scss"
+
+export default function intro() {
+    return (
+        <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/intro.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>WEBIT </h2>
+          <h1> by Chrisitian Maldonado</h1>
+          <h3> Take your imagination to the web</h3>
+        
+        
+        </div>
+        
+        
+        </div> 
+        </div>
+    )
+}
