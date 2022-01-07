34517d632311812d0a1a0e77cb59d3d69603e309
Christian Maldonado <41269975+christianlMaldonado@users.noreply.github.com>
2022-01-07 14:56:53 -0800

    Added first draft intro page

--- webit-site/src/components/intro/Intro.jsx
+++ webit-site/src/components/intro/Intro.jsx
@@ -1,18 +1,34 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-export default function intro() {
+export default function Intro() {
+    const textRef = useRef();
+    
+    useEffect(() => {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        backSpeed:60,
+        strings: ["Developer", "Designer", "Content Creator"],
+      });
+    }, []);
+    
     return (
-        <div className="intro" id="intro">
-      <div className="left">
-        <div className="imgContainer">
-          <img src="assets/intro.png" alt="" />
+      <div className="intro" id="intro">
+        <div className="left">
+          <div className="imgContainer">
+            <img src="assets/intro.png" alt="" />
+          </div>
         </div>
-      </div>
+      
       <div className="right">
         <div className="wrapper">
-          <h2>WebIT Consulting </h2>
-          <h1> by Chrisitian Maldonado</h1>
-          <h3> Take Your Imagination To The Web</h3>        
+          <h1>WebIT Consulting </h1>
+          <h2> Take Your Imagination To The Web </h2>
+          <h3>
+            Business <span ref={textRef}></span> 
+          </h3>        
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt=""/>
